893f0f134e2811846f95ccda088abfda384b1f98
Aaron Moore <aaron.mv.moore@gmail.com>
2023-02-15 16:55:05 -0600

    Corrected a mistake

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -14,14 +14,15 @@ len(a[a > 0])
 len(a[(a > 0) & (a % 2 == 0)])
 
 # If you were to add 3 to each data point, how many positive numbers would there be?
-len((a + 3)[a > 0])
+b = a + 3
+len(b[b > 0])
 
 # If you squared each number, what would the new mean and standard deviation be?
 (a**2).mean(), (a**2).std()
 
 # A common statistical operation on a dataset is centering. This means to adjust the data such that the mean of the data is 0. This is done by subtracting the mean from each data point. Center the data set. See this link for more on centering.
 centered = (a-a.mean())
-centered
+centered.mean()
 
 # Calculate the z-score for each data point. Recall that the z-score is given by:
 z_score = (a-a.mean())/a.std()
